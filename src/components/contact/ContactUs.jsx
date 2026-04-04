5c70699ff09807dcf5e9251199590a0ebfc0bf6e
moadel-01 <mohamed.adel2020559@gmail.com>
2026-04-04 03:17:15 +0200

    font & contact message input

--- src/components/contact/ContactUs.jsx
+++ src/components/contact/ContactUs.jsx
@@ -184,14 +184,14 @@ export default function ContactUs() {
                       Explain your message
                     </label>
                     <div className="mt-2">
-                      <input
+                      <textarea
                         id="message"
                         name="message"
                         type="text"
                         required
                         value={message}
                         onChange={(e) => setMessage(e.target.value)}
-                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        className="block w-full rounded-md bg-white h-30 resize-none px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                       />
                     </div>
                   </div>
